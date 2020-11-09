23006497be39765f9afcd6e8a8b8a86537c807ab
Azizeh <AZIZEP1@nationwide.com>
2020-11-09 07:57:51 -0600

    created CustomerRepositoryImpl and the CustomerRepositoryImpl test java files.

--- test/edu/cscc/CustomerRepositoryImplTest.java
+++ test/edu/cscc/CustomerRepositoryImplTest.java
@@ -1,5 +1,52 @@
+package edu.cscc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerRepositoryImplTest {
 
+    private CustomerRepositoryImpl customerrepo;
+
+    @BeforeEach
+    void setUp() {
+        customerrepo = new CustomerRepositoryImpl();
+    }
+
+    @Test
+    void create() throws DuplicateCustomerException {
+        Customer newcustomer = new Customer("Paul", "Stevens", "paul.stevens@gmail.com");
+        assertEquals(true, customerrepo.create(newcustomer).getAccountNumber() != null);
+    }
+
+    @Test
+    void read() throws DuplicateCustomerException {
+        Customer newcustomer = new Customer("Paul", "Stevens", "paul.stevens@gmail.com");
+        Customer returnCustomer = customerrepo.create(newcustomer);
+        assertEquals(returnCustomer, customerrepo.read(returnCustomer.getAccountNumber()));
+    }
+
+    @Test
+    void findByEmailAddress() throws DuplicateCustomerException {
+        Customer newcustomer = new Customer("Paul", "Stevens", "paul.stevens@gmail.com");
+        Customer returnCustomer = customerrepo.create(newcustomer);
+        assertEquals(returnCustomer, customerrepo.findByEmailAddress(returnCustomer.getEmailAddress()));
+    }
+
+    @Test
+    void update() throws DuplicateCustomerException, CustomerNotFoundException {
+        Customer newcustomer = new Customer("Paul", "Stevens", "paul.stevens@gmail.com");
+        Customer returnCustomer = customerrepo.create(newcustomer);
+        returnCustomer.setFirstName("Charles");
+        customerrepo.update(returnCustomer);
+        assertEquals("Charles", customerrepo.findByEmailAddress(returnCustomer.getEmailAddress()).getFirstName());
+    }
+
+    @Test
+    void delete() throws DuplicateCustomerException, CustomerNotFoundException {
+        Customer newcustomer = new Customer("Paul", "Stevens", "paul.stevens@gmail.com");
+        Customer returnCustomer = customerrepo.create(newcustomer);
+        assertEquals(true, customerrepo.delete(returnCustomer.getAccountNumber()));
+    }
 }
